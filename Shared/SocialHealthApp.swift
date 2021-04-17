d9fced7c5bb43ed4d0f4237996327908bf846da5
Flatout73 <fantom2259@gmail.com>
2021-04-17 15:52:44 +0300

    Initial Commit

---
+++ Shared/SocialHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialHealthApp.swift
+//  Shared
+//
+//  Created by Leonid Liadveikin on 17.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct SocialHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
